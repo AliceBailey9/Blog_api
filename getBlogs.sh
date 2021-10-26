b586673e82b1b2c8427d0a5331cc0f8b6ae2008f
Alice <alice.bailey@ecs.co.uk>
2021-10-26 18:21:21 +0100

    final script validations done

--- getBlogs.sh
+++ getBlogs.sh
@@ -2,11 +2,6 @@
 
 
 blogs="https://academy-project-blogs.s3-eu-west-1.amazonaws.com/teaching_code.doc https://academy-project-blogs.s3-eu-west-1.amazonaws.com/IDC.md https://academy-project-blogs.s3-eu-west-1.amazonaws.com/milestones.txt"
-# while IFS= read -r line
-# do
-#   echo "$line"
-# done < "$input"
-
 
 
 for b in $blogs
@@ -20,31 +15,14 @@ do
     CONTENTLENGTH= curl -sI ${b} | grep -i Content-Length | awk '{print $2}'
 
     if [ "$CONTENTLENGTH" != "0" ]; then 
-        echo "hello"
+        cd ./Data && { curl -O ${b} ; cd -; }
     fi
-    
-    # curl ${b} > ./Temp/output.txt
-    # check if output txt is empty 
-    # curl to data folder 
 done
 
+for file in ./Data/*.*
+do 
 
+filename="${file%.*}"
+mv -v "$file" "${filename}.txt"
 
-
-
-
-
-
-
-
-
-
-        # if [ -s $CONTENT  ]; then
-        # echo "not empty"
-        # fi
-# check if content is 0
-# check how many files in data and save as file.txt
-
-        # CONTENT=$(curl ${b})
-        # echo $CONTENT
-        # "cat $CONTENT"    
+done 
