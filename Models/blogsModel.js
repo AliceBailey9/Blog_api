eb4eacbd594e93d84cecdd36a8934be8a5b87c0a
Alice <alice.bailey@ecs.co.uk>
2021-10-26 17:37:10 +0100

    bash file

--- Models/blogsModel.js
+++ Models/blogsModel.js
@@ -7,7 +7,10 @@ const fetchAllBlogs = (cb) => {
       fs.readFile(`./Data/${blogFile}`, "utf8", (err, content) => {
         if (err) console.log(err);
         else {
-          allBlogs.push({ title: blogFile, content: content });
+          allBlogs.push({
+            title: blogFile.slice(0, blogFile.length - 4),
+            content: content,
+          });
           if (allBlogs.length === blogFiles.length) {
             cb(null, allBlogs);
           }
--- getBlogs.sh
+++ getBlogs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-blogs="https://academy-project-blogs.s3-eu-west-1.amazonaws.com/teaching_code.py https://academy-project-blogs.s3-eu-west-1.amazonaws.com/IDC.md https://academy-project-blogs.s3-eu-west-1.amazonaws.com/milestones.txt"
+blogs="https://academy-project-blogs.s3-eu-west-1.amazonaws.com/teaching_code.doc https://academy-project-blogs.s3-eu-west-1.amazonaws.com/IDC.md https://academy-project-blogs.s3-eu-west-1.amazonaws.com/milestones.txt"
 # while IFS= read -r line
 # do
 #   echo "$line"
@@ -11,27 +11,40 @@ blogs="https://academy-project-blogs.s3-eu-west-1.amazonaws.com/teaching_code.py
 
 for b in $blogs
 do
-echo cat ${b}
-    if [ "${b##*.}" == "py" ] || [  "${b##*.}" == "exe" ]; then
+    if [ ${b##*.} != "txt" ] && [ ${b##*.} != "md" ] && [ ${b##*.} != "doc" ]; then
         echo "This file format can not be uploaded"
-    else
-        CONTENT=$(curl ${b})
-        # echo $CONTENT
-        # "cat $CONTENT"    
-        if [ -s $CONTENT  ]; then
-        echo "not empty"
-        fi
+        continue 
     fi
+        echo "this worked"
+    
+    CONTENTLENGTH= curl -sI ${b} | grep -i Content-Length | awk '{print $2}'
+
+    if [ "$CONTENTLENGTH" != "0" ]; then 
+        echo "hello"
+    fi
+    
+    # curl ${b} > ./Temp/output.txt
+    # check if output txt is empty 
+    # curl to data folder 
 done
 
+
+
+
+
+
+
+
+
+
+
+
+        # if [ -s $CONTENT  ]; then
+        # echo "not empty"
+        # fi
 # check if content is 0
 # check how many files in data and save as file.txt
 
-# if [ -s blog1 ]; then
-#         .
-#         echo "the file is not-empty"
-# else
-        
-#         echo "the file is empty"
-  
-# fi
+        # CONTENT=$(curl ${b})
+        # echo $CONTENT
+        # "cat $CONTENT"    
